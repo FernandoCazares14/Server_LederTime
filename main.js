af2bd39928895a9982a6ba5aae086ed6196f2e02
FernandoCazares14 <proyectos.fernando.cazares@gmail.com>
2024-05-16 15:32:12 -0600

    update: Conexion simple con manychat

---
+++ main.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5060;
+
+let buffer = '';
+
+let opinion = {
+    "version": "v2",
+    "content": {
+        "messages": [{
+            "type":"text",
+            "text": "ESTO ES EXPRESS HDSPTM"
+        }]
+    },
+}
+
+app.post('/test', (req, res) => {
+    req.on('data', info => {
+        buffer += info.toString();
+    });
+
+    req.on('end', () => {
+        const data = JSON.parse(buffer);
+        console.log(data)
+        res.status(201).json(req.buffer)
+    });
+
+});
+
+app.get('/opinion', (req, res) => {
+    console.log("llegó la peticion");
+    console.log(req);
+    res.json(opinion);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server vivo en http://localhost:${PORT}`);
+});
